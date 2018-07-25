3f4ffb3dc666a1a31d71b6661465471cbbc6acbc
Spcktr <Spcktr@users.noreply.github.com>
2018-07-26 04:08:10 +1000

    added collect and read from file

--- src/social_analysis/ego.clj
+++ src/social_analysis/ego.clj
@@ -8,3 +8,23 @@
             [social_analysis.util :as u]
             me.raynes.fs :as fs)
   (:import [java.io File]))
+
+
+(defn read-edge-file
+  "reads and parses .edges file contents"
+  [filename]
+  (with-open [f (io/reader filename)]
+    (->>
+      f
+      line-seq
+      (r/map #(string/split % #"\s+"))
+      (r/map #(mapv (fn [x] (Long/parseLong x)) %))
+      (r/reduce #(g/add %1 (first %2) (second %2))
+              g/empty-graph))))
+
+(defn read-edge-files
+  "locates and reads file"
+  [ego-dir]
+  (r/reduce g/merge-graphs {}
+    (r/map read-edge-file
+      (fs/find-files ego-dir #".*\.edges$"))))
