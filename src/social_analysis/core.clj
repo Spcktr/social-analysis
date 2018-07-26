24135f9f6f2a20f6eee926add95413b2db205f69
Spcktr <Spcktr@users.noreply.github.com>
2018-07-26 23:48:35 +1000

    included namespaces

--- src/social_analysis/core.clj
+++ src/social_analysis/core.clj
@@ -1,5 +1,7 @@
 (ns social-analysis.core
-  (:gen-class))
+  (:use [social_analysis.graph :as g]
+        [social_analysis.ego :as ego]
+        [social_analysis.utils :as utils])
 
 (defn -main
   "I don't do a whole lot ... yet."
